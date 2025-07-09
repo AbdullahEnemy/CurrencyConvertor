96b9d3d5e517c2510b7a5517d8f92e765d692c66
AbdullahEnemy <akbuddy8@gmail.com>
2025-07-10 04:41:24 +0500

    app.js changed

---
+++ app.js
@@ -0,0 +1,5 @@
+const BASE_URL="https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/usd.json";
+const dropDowns=document.querySelectorALL(".dropdown select");
+for(code in countryList){
+    console.log(code);
+}
